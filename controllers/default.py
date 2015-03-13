f5a4c8ad8399e2e1a24e9837ab1e98a89c15e6a4
Henry Nguyen <henry@dxconcept.com>
2015-03-12 22:06:36 -0700

    Set default view to be /anagrams.

--- controllers/default.py
+++ controllers/default.py
@@ -7,5 +7,11 @@ Author: Henry Nguyen (henry@dxconcept.com)
 """
 
 def index():
+    redirect(URL('default', 'anagrams'))
     return dict(message=None)
 
+def anagrams():
+    return dict(message=None)
+
+def contactdb():
+    return dict(message=None)
